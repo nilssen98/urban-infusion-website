0d6a4b94774bb23e65ab49271416b5bd695a1c3c
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-02-24 20:02:49 +0100

    add material ui and an example

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,7 +1,29 @@
+import {Button, Typography} from "@mui/material";
+import {useState} from "react";
+
 export default function App() {
+    const [count, setCount] = useState<number>(0);
+
     return (
-        <div>
-            Hello world!
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '100vh'
+            }}
+        >
+            <Typography margin={2} variant={'h5'}>
+                Count: {count}
+            </Typography>
+            <Button
+                onClick={() => setCount((count) => count + 1)}
+                variant={'outlined'}
+                color={'secondary'}
+            >
+                Click me!
+            </Button>
         </div>
     )
 }
