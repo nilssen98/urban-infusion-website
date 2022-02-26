738b4a0c9a134f0e5aeb4be64da50ebda299fb4f
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-02-26 15:40:15 +0100

    fix typing on Section component

--- src/components/Wrappers/Section.tsx
+++ src/components/Wrappers/Section.tsx
@@ -1,19 +1,17 @@
-import {Grid, GridTypeMap} from "@mui/material";
+import {Grid} from "@mui/material";
 import {ReactNode} from "react";
-import {OverridableComponent} from "@mui/material/OverridableComponent";
 
-type Props = {
+interface Props {
     children?: ReactNode;
     height?: number;
     bgColor?: string;
     sx?: any;
-} & OverridableComponent<GridTypeMap>
+}
 
 export default function Section(props: Props) {
     return (
         <>
             <Grid
-                {...props}
                 container
                 sx={{
                     ...props.sx,
