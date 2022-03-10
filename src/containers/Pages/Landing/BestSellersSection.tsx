482f5bbe15515e715647ae41f1324c54423a7d08
Marko19907 <marko.stancic23@gmail.com>
2022-03-10 13:56:57 +0100

    Created an initial version of the best sellers section

---
+++ src/containers/Pages/Landing/BestSellersSection.tsx
@@ -0,0 +1,14 @@
+import Section from "../../../components/Wrappers/Section";
+
+export default function BestSellersSection() {
+
+    return (
+        <>
+            <Section
+                height={300}
+            >
+
+            </Section>
+        </>
+    );
+}
--- src/containers/Pages/Landing/Landing.tsx
+++ src/containers/Pages/Landing/Landing.tsx
@@ -2,6 +2,7 @@ import {Box, Divider, Typography} from "@mui/material";
 import Section from "../../../components/Wrappers/Section";
 import HeroSection from "./HeroSection";
 import FeaturesSection from "./FeaturesSection";
+import BestSellersSection from "./BestSellersSection";
 
 export default function Landing() {
     return (
@@ -11,10 +12,7 @@ export default function Landing() {
                 <Divider/>
                 <FeaturesSection/>
                 <Divider/>
-                <Section height={600}
-                         sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
-                    <Typography paddingTop={6} variant={'h4'}>Section</Typography>
-                </Section>
+                <BestSellersSection/>
                 <Divider/>
                 <Section height={600}
                          sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
