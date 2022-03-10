0ae43cd5e072fe28fb3d27e7cdc2c9751b784896
Marko19907 <marko.stancic23@gmail.com>
2022-03-10 21:19:12 +0100

    Completed the initial version of the best sellers section

--- src/containers/Pages/Landing/BestSellersSection.tsx
+++ src/containers/Pages/Landing/BestSellersSection.tsx
@@ -1,13 +1,55 @@
 import Section from "../../../components/Wrappers/Section";
+import {Box, Grid, Skeleton, Typography} from "@mui/material";
 
 export default function BestSellersSection() {
 
     return (
         <>
-            <Section
-                height={300}
-            >
-
+            <Section>
+                <Box
+                    sx={theme => ({
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        width: '100%'
+                    })}
+                >
+                    <Typography
+                        variant={'h3'}
+                        component={'h2'}
+                        sx={theme => ({
+                            textAlign: 'center',
+                            paddingBottom: 12
+                        })}
+                    >
+                        Best sellers
+                    </Typography>
+                    <Grid
+                        container
+                        gap={5}
+                        sx={theme => ({
+                            flexDirection: 'row',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            width: '100%',
+                        })}
+                    >
+                        {
+                            [0, 1, 2, 3].map((index: number) => {
+                                return (
+                                    <Skeleton
+                                        key={index}
+                                        variant="rectangular"
+                                        width={400}
+                                        height={420}
+                                    >
+                                    </Skeleton>
+                                );
+                            })
+                        }
+                    </Grid>
+                </Box>
             </Section>
         </>
     );
