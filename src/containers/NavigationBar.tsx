3d1d0d41fddeb2af8763e9d112ddbbef4dc62b4d
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-03-11 13:09:57 +0100

    make navigation bar responsive

--- src/containers/NavigationBar.tsx
+++ src/containers/NavigationBar.tsx
@@ -1,19 +1,44 @@
-import {AppBar, Box, Skeleton, Slide, Toolbar, Typography, useScrollTrigger} from "@mui/material";
+import {
+    AppBar,
+    Box,
+    IconButton,
+    Menu,
+    MenuItem,
+    Skeleton,
+    Slide,
+    Toolbar,
+    Typography,
+    useScrollTrigger
+} from "@mui/material";
 import Logo from "../components/Logo";
-import {ReactElement, ReactNode} from "react";
+import {ReactElement, ReactNode, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {ThemeSwitch} from "../components/ThemeSwitch";
 import {useSelector} from "react-redux";
 import {RootState, store} from "../state/store";
 import {userPreferencesSlice} from "../state/slices/userPreferences";
-import Section from "../components/Wrappers/Section";
+import MenuIcon from '@mui/icons-material/Menu';
+import {SxProps} from "@mui/system";
+import {Theme} from "@mui/material/styles";
 
 interface Props {
     children?: ReactElement;
 }
 
+const pages = ['Products', 'About'];
+
 export default function NavigationBar(props: Props) {
     const theme = useSelector((store: RootState) => store.userPreferences.theme);
+
+    const [anchorElNav, setAnchorElNav] = useState(null);
+
+    const handleOpenNavMenu = (event: any) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
     const navigate = useNavigate();
 
@@ -24,58 +49,88 @@ export default function NavigationBar(props: Props) {
     return (
         <>
             <HideOnScroll {...props}>
-                <AppBar
-                    color={'inherit'}
-                >
-                    <Toolbar>
+                <AppBar color={'inherit'}>
+                    <Toolbar sx={{px: 8}}>
                         <Box
                             sx={{
                                 display: 'flex',
-                                alignItems: 'center',
                                 flexWrap: 'nowrap',
+                                width: '100%',
+                                alignItems: 'center'
                             }}
                         >
-                            <Box
-                                sx={{
-                                    flex: 1,
-                                    display: 'flex',
-                                    justifyContent: 'left',
-                                }}
-                            >
+                            <Box sx={{flex: 1, display: {sm: 'flex', xs: 'none'}}}>
+                                <Logo clickable onClick={() => navigate('/')}/>
+                            </Box>
+                            <Box sx={{flex: 1, display: {sm: 'none', xs: 'flex'},}}>
+                                <IconButton
+                                    onClick={handleOpenNavMenu}
+                                >
+                                    <MenuIcon/>
+                                </IconButton>
+                            </Box>
+                            <Box sx={{flex: 1, display: {sm: 'none', xs: 'flex'}}}>
                                 <Logo clickable onClick={() => navigate('/')}/>
                             </Box>
                             <Box
                                 sx={{
-                                    display: 'flex',
-                                    flex: 3,
+                                    flex: 1,
+                                    display: {sm: 'flex', xs: 'none'},
                                     px: 4,
                                     justifyContent: 'right',
                                 }}
                             >
-                                <NavigationLink onClick={() => navigate('/products')}>
-                                    Products
-                                </NavigationLink>
+                                {
+                                    pages.map(page => (
+                                        <NavigationLink
+                                            sx={{pr: 8}}
+                                            key={page}
+                                            onClick={() => navigate(page.toLowerCase())}
+                                        >
+                                            {page}
+                                        </NavigationLink>
+                                    ))
+                                }
                             </Box>
                             <Box
                                 sx={{
+                                    flex: 1,
                                     display: 'flex',
                                     justifyContent: 'right',
                                     alignItems: 'center',
-                                    flex: 1,
+                                    justifySelf: 'right',
                                 }}
                             >
                                 <ThemeSwitch
                                     mode={theme}
                                     onClick={handleChangeTheme}
                                 />
-                                {/*<Skeleton
-                                    variant={'circular'}
-                                    width={48}
-                                    height={48}
-                                    animation={'wave'}
-                                    sx={{bgcolor: 'grey.900', cursor: 'pointer', marginLeft: '1rem'}}
-                                />*/}
                             </Box>
+                            <Menu
+                                anchorEl={anchorElNav}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                keepMounted
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                            >
+                                {
+                                    pages.map(page => (
+                                        <MenuItem key={page} onClick={() => {
+                                            navigate(page.toLowerCase());
+                                            handleCloseNavMenu()
+                                        }}>
+                                            <Typography>{page}</Typography>
+                                        </MenuItem>
+                                    ))
+                                }
+                            </Menu>
                         </Box>
                     </Toolbar>
                 </AppBar>
@@ -87,6 +142,7 @@ export default function NavigationBar(props: Props) {
 interface NavigationLinkProps {
     children?: ReactNode;
     onClick?: () => void;
+    sx?: SxProps<Theme>;
 }
 
 function NavigationLink(props: NavigationLinkProps) {
@@ -100,7 +156,8 @@ function NavigationLink(props: NavigationLinkProps) {
                     cursor: 'pointer',
                     '&:hover': {
                         textDecoration: 'underline'
-                    }
+                    },
+                    ...props.sx
                 }}
             >
                 {props.children}
