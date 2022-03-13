a4909b4dd35f2ff9e1a4578769acfaec2baa4f6c
Marko19907 <marko.stancic23@gmail.com>
2022-03-13 16:19:51 +0100

    Added text to the sections

--- src/containers/Pages/Landing/FeaturesSection.tsx
+++ src/containers/Pages/Landing/FeaturesSection.tsx
@@ -19,28 +19,67 @@ export default function FeaturesSection() {
             <Section>
                 <Box>
                     <Feature
-                        header={headerPlaceholder}
-                        body={bodyPlaceholder}
+                        flipped
+                        header={'Heaviest drinkers'}
+                        body={'China, Sri Lanka, and Kenya export the most tea worldwide, shipping out about '
+                            + '1 million metric tons of it among the three countries. Thanks in large part to it’s '
+                            + 'enormous population, China consumes the most of any country in total. '
+                            + 'However, per capita, the United Kingdom, Ireland and Turkey take the top '
+                            + 'three spots. Over 3 billion teacups are consumed worldwide each year.'}
+                        image_url={'https://i.imgur.com/CV3NL2Z.jpg'}
+                        onClick={() => navigate('/products')}
+                    />
+                    <Feature
+                        header={'Black tea'}
+                        body={'Comforting flavors of honey, stone fruits, or roasted almonds and much more. \n '
+                            + 'The nutrients in black tea may reduce the risk of cancer, protect the heart against '
+                            + 'atherosclerosis, and help maintain a healthy blood pressure. \n '
+                            + 'Black tea is the most consumed tea across the world; in the US, '
+                            + '86% of the tea enjoyed throughout 2017 was black tea.'}
                         image_url={imageURL + "&" + Math.random()}
                         onClick={() => navigate('/products')}
                     />
                     <Feature
                         flipped
-                        header={headerPlaceholder}
-                        body={bodyPlaceholder}
+                        header={'Green tea'}
+                        body={'A verdant flavor alternative with often hints of vegetal, marine, '
+                            + 'and freshly cut grass. \n Studies suggest that green tea may have positive effects on '
+                            + 'weight loss, liver disorders, type 2 diabetes, Alzheimer’s disease, and more. '
+                            + 'Ancient cultures used green tea as medicine to control bleeding, heal wounds, '
+                            + 'aid digestion, and to improve heart and mental health. \n '
+                            + 'Green tea is an exceptionally popular choice amongst both everyday and more '
+                            + 'enthusiastic tea drinkers in the United States, likely due to numerous media '
+                            + 'outlets touting its health benefits.'}
                         image_url={imageURL + "&" + Math.random()}
                         onClick={() => navigate('/products')}
                     />
                     <Feature
-                        header={headerPlaceholder}
-                        body={bodyPlaceholder}
+                        header={'White tea'}
+                        body={'A mythical and elusive verity light and subtle in flavor with often floral '
+                            + 'sweet notes. \n '
+                            + 'White tea has been linked to many potential health benefits. These include '
+                            + 'improving skin and hair health, reducing the risk of cancer, reducing inflammation, '
+                            + 'improving brain health, reducing insulin resistance, lowering the risk of heart '
+                            + 'disease, and improving oral health. \n '
+                            + 'White tea is the least processed of all '
+                            + 'teas, most white teas are simply picked and withered. Therefore, white tea '
+                            + 'contains elevated levels of antioxidants that are present in fresh tea leaves.'}
                         image_url={imageURL + "&" + Math.random()}
                         onClick={() => navigate('/products')}
                     />
                     <Feature
                         flipped
-                        header={headerPlaceholder}
-                        body={bodyPlaceholder}
+                        header={'Dark tea'}
+                        body={'This brew shows an amber color, with a little sticky feeling, mellow and smooth, '
+                            + 'and a unique sweetness. The marvelous taste will make you hooked and can’t '
+                            + 'stop drinking. \n '
+                            + 'Dark tea may have similar benefits to green tea. The nutrients in it '
+                            + 'may reduce the risk of several cancers, protect the heart, and help maintain healthy '
+                            + 'blood pressure. \n Many presume that “dark” tea and “black” tea are the same, but '
+                            + 'there is a world of difference separating these two categories. \n '
+                            + 'Darks teas are closer to green teas in that they are processed like green teas '
+                            + 'initially. Then, they go through a bacterial fermentation process and are aged for '
+                            + 'many years to come.'}
                         image_url={imageURL + "&" + Math.random()}
                         onClick={() => navigate('/products')}
                     />
@@ -108,6 +147,7 @@ function Feature(props: FeatureProps) {
                     <Typography
                         my={8}
                         sx={theme => ({
+                            whiteSpace: 'pre-line',
                             [theme.breakpoints.down('md')]: {
                                 textAlign: "center"
                             }
