204d5082e612e8589653b93e39132e3357c11a22
Marko19907 <marko.stancic23@gmail.com>
2022-03-16 20:48:15 +0100

    Added the product card

---
+++ src/containers/Pages/Products/ProductCard.tsx
@@ -0,0 +1,64 @@
+import {Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Typography} from '@mui/material';
+
+interface ProductCardProps {
+    title?: string;
+    price?: string;
+    image_url?: string;
+}
+
+export default function ProductCard(props: ProductCardProps) {
+
+    return (
+        <>
+            <Card
+                sx={{
+                    width: 250,
+                    height: 400,
+                }}
+            >
+                <CardActionArea>
+                    <CardMedia
+                        component='img'
+                        height={250}
+                        image={props.image_url}
+                    />
+                    <CardContent>
+                        <Typography
+                            gutterBottom
+                            variant='h5'
+                            textAlign={'center'}
+                        >
+                            {
+                                props.title
+                            }
+                        </Typography>
+                        <Typography
+                            variant={'body1'}
+                            textAlign={'center'}
+                            color='primary'
+                            paddingBottom={1}
+                        >
+                            {
+                                props.price
+                            }
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <Button
+                        variant='outlined'
+                        size='small'
+                    >
+                        Add to cart
+                    </Button>
+                </CardActions>
+            </Card>
+        </>
+    );
+}
--- src/containers/Pages/Products/ProductsList.tsx
+++ src/containers/Pages/Products/ProductsList.tsx
@@ -1,5 +1,5 @@
-import {useParams} from 'react-router-dom';
 import {Typography} from '@mui/material';
+import ProductCard from './ProductCard';
 
 interface Props {
     id?: string;
@@ -9,6 +9,11 @@ export function ProductsList(props: Props) {
     return (
         <>
             <Typography>{props.id}</Typography>
+            <ProductCard
+                title={'Title'}
+                price={'$9.99'}
+                image_url={'https://i.imgur.com/ZG4W7Le.jpg'}
+            />
         </>
     );
 }
