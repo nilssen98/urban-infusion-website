9489eb06c5273f716dd2536c9a311c83f127e534
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-03-16 20:03:02 +0100

    add clickable chips for different feature descriptions

--- src/containers/Pages/Landing/FeaturesSection.tsx
+++ src/containers/Pages/Landing/FeaturesSection.tsx
@@ -1,23 +1,18 @@
-import {Box, Button, Grid, Typography} from '@mui/material';
+import {Box, Button, Chip, Grid, Typography, useTheme} from '@mui/material';
 import {useNavigate} from 'react-router-dom';
 import Section from '../../../components/Wrappers/Section';
+import {useState} from "react";
 
 export default function FeaturesSection() {
     const imageURL: string = 'https://picsum.photos/570/350?grayscale';
-    const headerPlaceholder: string = 'Benefits of green tea';
-    const bodyPlaceholder: string =
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
-        + 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-        + 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris'
-        + 'nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in'
-        + 'reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla'
-        + 'pariatur.';
+
     const navigate = useNavigate();
 
     return (
         <>
             <Section>
                 <Box>
+                    {/*
                     <Feature
                         flipped
                         header={'Heaviest drinkers'}
@@ -28,58 +23,46 @@ export default function FeaturesSection() {
                             + 'three spots. Over 3 billion teacups are consumed worldwide each year.'}
                         image_url={'https://i.imgur.com/CV3NL2Z.jpg'}
                         onClick={() => navigate('/products')}
-                    />
+                    />*/}
                     <Feature
                         header={'Black tea'}
-                        body={'Comforting flavors of honey, stone fruits, or roasted almonds and much more. \n '
-                            + 'The nutrients in black tea may reduce the risk of cancer, protect the heart against '
-                            + 'atherosclerosis, and help maintain a healthy blood pressure. \n '
-                            + 'Black tea is the most consumed tea across the world; in the US, '
-                            + '86% of the tea enjoyed throughout 2017 was black tea.'}
+                        description={{
+                            flavor: 'Comforting flavors of honey, stone fruits, or roasted almonds and much more',
+                            benefits: 'The nutrients in black tea may reduce the risk of cancer, protect the heart against atherosclerosis, and help maintain a healthy blood pressure.',
+                            fact: 'Black tea is the most consumed tea across the world; in the US, 86% of the tea enjoyed throughout 2017 was black tea.'
+                        }}
                         image_url={imageURL + '&' + Math.random()}
                         onClick={() => navigate('/products')}
                     />
                     <Feature
                         flipped
                         header={'Green tea'}
-                        body={'A verdant flavor alternative with often hints of vegetal, marine, '
-                            + 'and freshly cut grass. \n Studies suggest that green tea may have positive effects on '
-                            + 'weight loss, liver disorders, type 2 diabetes, Alzheimer’s disease, and more. '
-                            + 'Ancient cultures used green tea as medicine to control bleeding, heal wounds, '
-                            + 'aid digestion, and to improve heart and mental health. \n '
-                            + 'Green tea is an exceptionally popular choice amongst both everyday and more '
-                            + 'enthusiastic tea drinkers in the United States, likely due to numerous media '
-                            + 'outlets touting its health benefits.'}
+                        description={{
+                            flavor: 'A verdant flavor alternative with often hints of vegetal, marine, and freshly cut grass.',
+                            benefits: 'Studies suggest that green tea may have positive effects on weight loss, liver disorders, type 2 diabetes, Alzheimer’s disease, and more.',
+                            fact: 'Ancient cultures used green tea as medicine to control bleeding, heal wounds, aid digestion, and to improve heart and mental health.'
+                        }}
                         image_url={imageURL + '&' + Math.random()}
                         onClick={() => navigate('/products')}
                     />
                     <Feature
                         header={'White tea'}
-                        body={'A mythical and elusive verity light and subtle in flavor with often floral '
-                            + 'sweet notes. \n '
-                            + 'White tea has been linked to many potential health benefits. These include '
-                            + 'improving skin and hair health, reducing the risk of cancer, reducing inflammation, '
-                            + 'improving brain health, reducing insulin resistance, lowering the risk of heart '
-                            + 'disease, and improving oral health. \n '
-                            + 'White tea is the least processed of all '
-                            + 'teas, most white teas are simply picked and withered. Therefore, white tea '
-                            + 'contains elevated levels of antioxidants that are present in fresh tea leaves.'}
+                        description={{
+                            flavor: 'A mythical and elusive verity light and subtle in flavor with often floral sweet notes.',
+                            benefits: 'White tea has been linked to many potential health benefits. These include improving skin and hair health, reducing the risk of cancer, reducing inflammation, improving brain health, reducing insulin resistance, lowering the risk of heart disease, and improving oral health.',
+                            fact: 'White tea is the least processed of all teas, most white teas are simply picked and withered. Therefore, white tea contains elevated levels of antioxidants that are present in fresh tea leaves.'
+                        }}
                         image_url={imageURL + '&' + Math.random()}
                         onClick={() => navigate('/products')}
                     />
                     <Feature
                         flipped
                         header={'Dark tea'}
-                        body={'This brew shows an amber color, with a little sticky feeling, mellow and smooth, '
-                            + 'and a unique sweetness. The marvelous taste will make you hooked and can’t '
-                            + 'stop drinking. \n '
-                            + 'Dark tea may have similar benefits to green tea. The nutrients in it '
-                            + 'may reduce the risk of several cancers, protect the heart, and help maintain healthy '
-                            + 'blood pressure. \n Many presume that “dark” tea and “black” tea are the same, but '
-                            + 'there is a world of difference separating these two categories. \n '
-                            + 'Darks teas are closer to green teas in that they are processed like green teas '
-                            + 'initially. Then, they go through a bacterial fermentation process and are aged for '
-                            + 'many years to come.'}
+                        description={{
+                            flavor: 'This brew shows an amber color, with a little sticky feeling, mellow and smooth, and a unique sweetness. The marvelous taste will make you hooked and can’t stop drinking.',
+                            benefits: 'Dark tea may have similar benefits to green tea. The nutrients in it may reduce the risk of several cancers, protect the heart, and help maintain healthy blood pressure.',
+                            fact: 'Many presume that “dark” tea and “black” tea are the same, but initially. Then, they go through a bacterial fermentation process and are aged for many years to come.'
+                        }}
                         image_url={imageURL + '&' + Math.random()}
                         onClick={() => navigate('/products')}
                     />
@@ -92,12 +75,16 @@ export default function FeaturesSection() {
 interface FeatureProps {
     flipped?: boolean;
     header?: string;
-    body?: string;
     image_url?: string;
     onClick?: () => void;
+    description: Record<string, string>
 }
 
 function Feature(props: FeatureProps) {
+    const [currentItem, setCurrentItem] = useState<string>(Object.keys(props.description)[0]);
+
+    const theme = useTheme();
+
     return (
         <>
             <Grid
@@ -110,11 +97,7 @@ function Feature(props: FeatureProps) {
                     justifyContent: 'center',
                 }}
             >
-                <Grid
-                    md={6}
-                    item
-                    container
-                >
+                <Grid md={6} item container>
                     <img
                         src={props.image_url}
                         style={{width: '100%', minWidth: 1}}
@@ -126,35 +109,49 @@ function Feature(props: FeatureProps) {
                     item
                     container
                     sx={{
-                        justifyContent: {xs: 'center', md: 'flex-start'}
+                        justifyContent: {xs: 'center', md: 'flex-start'},
+                        flexDirection: 'column',
                     }}
                 >
                     <Typography
+                        pb={4}
                         variant={'h4'}
                         component={'h3'}
-                        sx={{
-                            textAlign: {xs: 'center', md: 'left'}
-                        }}
+                        sx={{textAlign: {xs: 'center', md: 'left'}}}
                     >
-                        {
-                            props.header
-                        }
+                        {props.header}
                     </Typography>
+                    <Box sx={{mb: 4, alignSelf: {md: 'start', xs: 'center'}}}>
+                        {
+                            Object.keys(props.description).map((key, i) => (
+                                <Chip
+                                    key={i}
+                                    label={key}
+                                    variant={'outlined'}
+                                    onClick={() => setCurrentItem(key)}
+                                    sx={{
+                                        mr: 1,
+                                        alignSelf: {md: 'start', xs: 'center'},
+                                        textTransform: 'capitalize',
+                                        borderColor: currentItem === key ? theme.palette.primary.main : null
+                                    }}
+                                />
+                            ))
+                        }
+                    </Box>
                     <Typography
-                        my={8}
                         sx={{
                             whiteSpace: 'pre-line',
                             textAlign: {xs: 'center', md: 'left'}
                         }}
                     >
-                        {
-                            props.body
-                        }
+                        {props.description[currentItem]}
                     </Typography>
                     <Box
                         sx={{
+                            pt: 4,
                             display: 'flex',
-                            flex: 1,
+                            width: '100%',
                             justifyContent: {xs: 'center', md: `${props.flipped ? 'flex-start' : 'flex-end'}`}
                         }}
                     >
