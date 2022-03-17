02c0f24171e0d850dbcf65e12f28f829cb5985e3
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-03-17 12:15:32 +0100

    add persistance to the redux state

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -6,27 +6,32 @@ import Footer from '../Footer';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Products from '../Pages/Products/Products';
 import NotFound from '../Pages/NotFound/NotFound';
-import {RootState} from '../../state/store';
-import {useSelector} from 'react-redux';
+import {persistor, RootState, store} from '../../state/store';
+import {Provider, useSelector} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
 export default function App() {
-    const theme = useSelector((store: RootState) => store.userPreferences.theme);
+    const theme = useSelector((s: RootState) => s.userPreferences.theme);
 
     return (
-        <ThemeProvider theme={getTheme(theme)}>
-            <CssBaseline/>
-            <BrowserRouter>
-                <Toolbar/>
-                <NavigationBar/>
-                <Routes>
-                    <Route path={'/'} element={<Landing/>}/>
-                    <Route path={'/products'} element={<Products/>}/>
-                    <Route path={'/products/:id'} element={<Products/>}/>
-                    <Route path={'*'} element={<NotFound/>}/>
-                </Routes>
-                <Divider/>
-                <Footer/>
-            </BrowserRouter>
-        </ThemeProvider>
+        <Provider store={store}>
+            <PersistGate persistor={persistor} loading={<div>Loading</div>}>
+                <ThemeProvider theme={getTheme(theme)}>
+                    <CssBaseline/>
+                    <BrowserRouter>
+                        <Toolbar/>
+                        <NavigationBar/>
+                        <Routes>
+                            <Route path={'/'} element={<Landing/>}/>
+                            <Route path={'/products'} element={<Products/>}/>
+                            <Route path={'/products/:id'} element={<Products/>}/>
+                            <Route path={'*'} element={<NotFound/>}/>
+                        </Routes>
+                        <Divider/>
+                        <Footer/>
+                    </BrowserRouter>
+                </ThemeProvider>
+            </PersistGate>
+        </Provider>
     );
 }
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,4 +1,6 @@
-import {combineReducers, configureStore, EnhancedStore} from '@reduxjs/toolkit';
+import {combineReducers, createStore, EnhancedStore} from '@reduxjs/toolkit';
+import {persistReducer, persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import {UserPreferences, userPreferencesSlice} from './slices/userPreferences';
 
 // Root state that combines all the state types from the slices
@@ -7,12 +9,19 @@ export interface RootState {
 }
 
 // Root reducers that combines all the available reducers
-const rootReducers = combineReducers({
+const rootReducer = combineReducers({
     userPreferences: userPreferencesSlice.reducer
 });
 
+const persistConfig = {
+    key: 'root',
+    storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 // Create the store instance in which we specify our reducers
 // In this case, its the combination of all our reducers
-export const store: EnhancedStore<RootState> = configureStore({
-    reducer: rootReducers
-});
+export const store: EnhancedStore<RootState> = createStore(persistedReducer);
+
+export const persistor = persistStore(store);
