20e67dd0269137a0c8458938429676b9f6b6a599
Marko19907 <marko.stancic23@gmail.com>
2022-03-22 00:17:10 +0100

    Improved the NotFound page

--- src/containers/Pages/NotFound/NotFound.tsx
+++ src/containers/Pages/NotFound/NotFound.tsx
@@ -1,13 +1,38 @@
-import {Grid, Typography} from '@mui/material';
+import {Box, Typography} from '@mui/material';
 import Section from '../../../components/Wrappers/Section';
 
 export default function NotFound() {
     return (
         <>
-            <Section height={800}>
-                <Grid container sx={{justifyContent: 'center', alignItems: 'center', height: '100%'}}>
-                    <Typography variant={'h4'}>There is nothing here!</Typography>
-                </Grid>
+            <Section
+                height={600}
+                sx={{
+                    margin: 0
+                }}
+            >
+                <Box
+                    display={'flex'}
+                    sx={{
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        flexDirection: 'column',
+                        width: '100%',
+                        textAlign: 'center',
+                    }}
+                >
+                    <Typography variant={'h1'} paddingBottom={6}>
+                        404
+                    </Typography>
+                    <Typography variant={'h3'} component={'h2'} paddingBottom={1}>
+                        Oops! That Page Can’t Be Found
+                    </Typography>
+                    <Typography variant={'h6'} component={'h3'} paddingBottom={4}>
+                        The page you are looking for does not exist
+                    </Typography>
+                    <Typography variant={'body1'} component={'h4'}>
+                        Please return to the home page
+                    </Typography>
+                </Box>
             </Section>
         </>
     );
