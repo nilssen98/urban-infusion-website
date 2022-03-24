aa32da3a27f3e3941e110079107b01ab3946803d
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-03-24 14:53:26 +0100

    change typography to box

--- src/components/Pages/Cart/CartItemList.tsx
+++ src/components/Pages/Cart/CartItemList.tsx
@@ -1,7 +1,7 @@
 import {ProductDto} from '../../../api/urbaninfusion/dto/product-dto';
 import {store} from '../../../state/store';
 import CartItem from './CartItem';
-import {Divider, Stack, Typography} from '@mui/material';
+import {Box, Divider, Stack, Typography} from '@mui/material';
 
 interface Props {
     items: ProductDto[];
@@ -12,8 +12,8 @@ export default function CartItemList(props: Props) {
         <>
             <Stack direction={'column'} width={'100%'}>
                 <Stack px={4} direction={'row'} mb={2} alignContent={'flex-end'} textAlign={'right'}>
-                    <Typography flex={1}></Typography>
-                    <Typography flex={1}></Typography>
+                    <Box flex={1}/>
+                    <Box flex={1}/>
                     <Typography flex={1} variant={'h6'}>Count</Typography>
                     <Typography flex={1} variant={'h6'}>Total Price</Typography>
                 </Stack>
