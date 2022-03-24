34caa9e7dbc11971e42ae2ae1c6797277b27ed82
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-03-24 10:53:47 +0100

    remove error from redux

--- src/containers/Pages/Cart.tsx
+++ src/containers/Pages/Cart.tsx
@@ -3,15 +3,10 @@ import {store} from '../../state/store';
 import CartItem from '../../components/Pages/Cart/CartItem';
 
 export default function Cart() {
-    const cartItems = store.getState().cart.items;
-
     return (
         <>
             <Section>
                 {
-                    cartItems.map(item => (
-                        <CartItem item={item}/>
-                    ))
                 }
             </Section>
         </>
