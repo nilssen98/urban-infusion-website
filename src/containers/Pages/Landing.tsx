1ea516ae7ee70a239e85da354a22e61875dcf0f8
Marko19907 <marko.stancic23@gmail.com>
2022-03-24 11:12:12 +0100

    The hero section is now reusable

--- src/containers/Pages/Landing.tsx
+++ src/containers/Pages/Landing.tsx
@@ -1,15 +1,22 @@
-import {Box, Divider, Typography} from '@mui/material';
-import Section from '../../components/Wrappers/Section';
-import HeroSection from '../../components/Pages/Landing/HeroSection';
+import {Box, Divider} from '@mui/material';
+import HeroSection from '../../components/HeroSection';
 import FeaturesSection from '../../components/Pages/Landing/FeaturesSection';
 import FeaturedProductsSection from '../../components/Pages/Landing/FeaturedProductsSection';
 import TestimonialSection from '../../components/Pages/Landing/TestimonialSection';
+import BackgroundImage from '../../assets/images/hero-section.jpg';
 
 export default function Landing() {
     return (
         <>
             <Box>
-                <HeroSection/>
+                <HeroSection
+                    title={'Find your herbal friend'}
+                    description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
+                    backgroundUrl={BackgroundImage}
+                    // backgroundUrlMobile={'https://i.imgur.com/cGWuYIr.jpg'} // mobile 1
+                    // backgroundUrlMobile={'https://i.imgur.com/tUtZZfJ.jpg'} // mobile 2
+                    backgroundUrlMobile={'https://i.imgur.com/fBLkybq.jpg'} // mobile 3
+                />
                 <Divider/>
                 <FeaturedProductsSection/>
                 <Divider/>
