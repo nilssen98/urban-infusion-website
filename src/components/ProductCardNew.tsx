4b2ce003f52e0256075645bd84253dd110c2d814
Marko19907 <marko.stancic23@gmail.com>
2022-03-28 21:35:30 +0200

    Created an initial version of the new product card

--- src/components/ProductCardNew.tsx
+++ src/components/ProductCardNew.tsx
@@ -1,3 +1,5 @@
+import {Box, Button, CardActionArea, Grow, Typography} from '@mui/material';
+import {useState} from 'react';
 
 interface Props {
     title?: string;
@@ -10,8 +12,82 @@ ProductCardNew.defaultProps = {
 };
 
 export default function ProductCardNew(props: Props) {
+    const [onCardHover, setOnCardHover] = useState<boolean>(false);
+
     return (
         <>
+            <CardActionArea
+                component={'div'}
+                onMouseEnter={() => setOnCardHover(!onCardHover)}
+                onMouseLeave={() => setOnCardHover(!onCardHover)}
+                sx={{
+                    height: 320,
+                    width: 200,
+                    boxShadow: 1,
+                    transition: 'transform 0.2s ease-in-out',
+                    '&:hover': {transform: 'scale3d(1.025, 1.025, 1)', boxShadow: 3},
+                }}
+            >
+                <Box
+                    height={'inherit'}
+                    width={'inherit'}
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Box
+                        height={200}
+                        sx={{
+                            objectFit: 'contain',
+                        }}
+                    >
+                        <img
+                            src={props.image_url}
+                            alt={'Product image'}
+                            style={{objectFit: 'contain', height: 'inherit'}}
+                            draggable={false}
+                        />
+                    </Box>
+                    <Box paddingBottom={6}>
+                        <Typography
+                            variant={'h4'}
+                            paddingBottom={2}
+                            textAlign={'center'}
+                            sx={{
+                                userSelect: 'text',
+                            }}
+                        >
+                            {props.title}
+                        </Typography>
+                        <Typography
+                            variant={'h5'}
+                            textAlign={'center'}
+                            sx={{
+                                userSelect: 'text',
+                            }}
+                        >
+                            ${props.price}
+                        </Typography>
+                        {
+                            onCardHover ? (
+                                <Grow in={onCardHover}>
+                                    <Button
+                                        variant={'contained'}
+                                        size={'small'}
+                                    >
+                                        Add to cart
+                                    </Button>
+                                </Grow>
+                            ) : (
+                                <></>
+                            )
+                        }
+                    </Box>
+                </Box>
+            </CardActionArea>
         </>
     );
 }
