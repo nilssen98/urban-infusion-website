869e1f9de5dce8b4fa47dc44a92bc48e23bece03
Marko19907 <marko.stancic23@gmail.com>
2022-03-28 23:36:51 +0200

    Improved the products list on the product page

--- src/components/Pages/Products/ProductsList.tsx
+++ src/components/Pages/Products/ProductsList.tsx
@@ -1,5 +1,7 @@
 import ProductCard from '../../ProductCard';
 import {ProductDto} from '../../../api/urbaninfusion/dto/product-dto';
+import {Box, Grid} from '@mui/material';
+import {range} from 'lodash-es';
 
 interface Props {
     id?: string;
@@ -7,23 +9,78 @@ interface Props {
 }
 
 export function ProductsList(props: Props) {
+
+    function gridWrapper(element: JSX.Element): JSX.Element {
+        return (
+            <>
+                <Grid
+                    container
+                    item
+                    xs={12} sm={6} lg={1}
+                    sx={{
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    {element}
+                </Grid>
+            </>
+        );
+    }
+
     return (
         <>
-            <ProductCard
-                title={'Title'}
-                price={9.99}
-                image_url={'https://i.imgur.com/ZG4W7Le.jpg'}
-            />
             {
-                props.products.map(product =>
-                    <ProductCard
-                        key={product.id}
-                        title={product.title}
-                        price={product.price}
-                        image_url={'https://i.imgur.com/ZG4W7Le.jpg'}
-                    />
-                )
+                /*
+            <Grid
+                container
+                paddingTop={8}
+                paddingBottom={8}
+                sx={{
+                    justifyContent: 'space-evenly',
+                    alignItems: 'center',
+                    width: '100%',
+                    rowGap: 12,
+                    columnGap: 12,
+                }}
+            >
+            </Grid>
+                */
             }
+            <Box
+                paddingTop={8}
+                paddingBottom={8}
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'space-evenly',
+                    alignItems: 'center',
+                    flexWrap: 'wrap',
+                    width: '100%',
+                    rowGap: 12,
+                    columnGap: 12,
+                }}
+            >
+                {
+                    range(3).map(item =>
+                        <ProductCard
+                            key={item}
+                            title={'Title'}
+                            price={9.99}
+                            image_url={'https://i.imgur.com/ZG4W7Le.jpg'}
+                        />
+                    )
+                }
+                {
+                    props.products.map(product =>
+                        <ProductCard
+                            key={product.id}
+                            title={product.title}
+                            price={product.price}
+                            image_url={'https://i.imgur.com/ZG4W7Le.jpg'}
+                        />
+                    )
+                }
+            </Box>
         </>
     );
 }
