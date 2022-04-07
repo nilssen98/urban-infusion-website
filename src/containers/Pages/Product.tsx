8f4fd02f54c188f69ac8fb549df1b07d1ce308bf
Marko19907 <marko.stancic23@gmail.com>
2022-04-07 20:54:15 +0200

    Added the description to the Product page

--- src/containers/Pages/Product.tsx
+++ src/containers/Pages/Product.tsx
@@ -27,20 +27,6 @@ export default function Product(props: Props) {
             <Page isLoading={isLoading}>
                 <Section>
                     {
-                        data && <Stack direction={'column'} spacing={4}>
-                            <Typography variant={'h3'}>This is the product page!</Typography>
-                            <Typography variant={'h5'}>id: {data.id}</Typography>
-                            <Typography variant={'h5'}>title: {data.title}</Typography>
-                            <Typography variant={'h5'}>description: {data.description}</Typography>
-                            <Typography variant={'h5'}>category: {data.category}</Typography>
-                            <Typography variant={'h5'}>price: {data.price}</Typography>
-                            <Typography variant={'h5'}>discount: {data.discount}</Typography>
-                            <Typography variant={'h5'}>weight: {data.weight}</Typography>
-                        </Stack>
-                    }
-                </Section>
-                <Section>
-                    {
                         data && (
                             <Stack
                                 direction={{xs: 'column', md: 'row'}}
@@ -88,6 +74,16 @@ export default function Product(props: Props) {
                                         Add to cart
                                     </Button>
                                 </Stack>
+                            </Stack>
+                        )
+                    }
+                </Section>
+                <Section>
+                    {
+                        data && (
+                            <Stack>
+                                <Typography variant={'h3'} marginBottom={2}>Description</Typography>
+                                <Typography variant={'h5'}>{data.description}</Typography>
                             </Stack>
                         )
                     }
