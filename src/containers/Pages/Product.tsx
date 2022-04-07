1f36d4ef8b0c6f6abaddbf39712ea25b72a1d1cc
Marko19907 <marko.stancic23@gmail.com>
2022-04-07 13:01:37 +0200

    Added an add to cart button to the Product page

--- src/containers/Pages/Product.tsx
+++ src/containers/Pages/Product.tsx
@@ -2,8 +2,9 @@ import {useParams} from 'react-router-dom';
 import {useQuery} from 'react-query';
 import {getProductById} from '../../api/urbaninfusion/public/products';
 import Section from '../../components/Wrappers/Section';
-import {Box, Stack, Typography} from '@mui/material';
+import {Box, Button, Stack, Typography} from '@mui/material';
 import Page from '../../components/Wrappers/Page';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 interface Props {
     image_url?: string;
@@ -42,7 +43,7 @@ export default function Product(props: Props) {
                     {
                         data && (
                             <Stack
-                                direction={'row'}
+                                direction={{xs: 'column', md: 'row'}}
                                 width={'100%'}
                                 sx={{
                                     border: '1px solid red',
@@ -57,24 +58,35 @@ export default function Product(props: Props) {
                                     <img
                                         src={props.image_url}
                                         alt={'Product image'}
+                                        draggable={false}
                                         style={{objectFit: 'contain', height: 'inherit'}}
                                     />
                                 </Box>
                                 <Stack
                                     direction={'column'}
-                                    justifyContent={'flex-start'}
-                                    alignItems={'end'}
+                                    justifyContent={'center'}
+                                    alignItems={'center'}
                                     sx={{
                                         flexGrow: '1',
-                                        width: '100%',
                                         border: '1px solid blue',
                                     }}
                                 >
                                     <Typography variant={'h1'} marginBottom={4}>{data.title}</Typography>
-                                    <Typography variant={'h2'} marginBottom={8}>{`${data.price}$ / ${data.weight}`}</Typography>
-                                    <Stack>
-                                        <Typography variant={'h4'} marginBottom={4}>Quantity: </Typography>
+                                    <Typography variant={'h3'} marginBottom={8}>{`${data.price}$ / ${data.weight}`}</Typography>
+                                    <Stack
+                                        direction={'row'}
+                                    >
+                                        <Typography variant={'h5'} marginBottom={4}>Quantity: </Typography>
+                                        <Typography variant={'h5'} color={'red'}>Quantity selector goes here</Typography>
                                     </Stack>
+                                    <Button
+                                        variant={'contained'}
+                                        size={'large'}
+                                        startIcon={<AddShoppingCartIcon/>}
+                                        sx={{width: '80%'}}
+                                    >
+                                        Add to cart
+                                    </Button>
                                 </Stack>
                             </Stack>
                         )
