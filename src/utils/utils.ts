0075b466ce80313149ac24a8ae7cb31f8e489d8c
Marko19907 <marko.stancic23@gmail.com>
2022-04-09 19:39:55 +0200

    Added a date formatting utility

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,19 @@
+const monthNames = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+];
+
+
 export function hexToRgb(hex: string): number[] {
     return hex
         .slice(1)
@@ -5,4 +21,29 @@ export function hexToRgb(hex: string): number[] {
         .match(/^(..)(..)(..)$/)!
         .slice(1, 4)
         .map(s => parseInt(s, 16));
+}
+
+export function formatDate(dateString: string): string {
+    if (!Date.parse(dateString)) {
+        return '';
+    }
+
+    const date = new Date(dateString);
+    return monthNames[date.getMonth()]
+        + ' ' + date.getDate()
+        + dateOrdinal(date.getDate())
+        + ', ' + date.getFullYear();
+}
+
+function dateOrdinal(date: number): string {
+    switch (date) {
+        case 1: case 21: case 31:
+            return 'st';
+        case 2: case 22:
+            return 'nd';
+        case 3: case 23:
+            return 'rd';
+        default:
+            return 'th';
+    }
 }
