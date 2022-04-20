5e7431c486d14fa6fc3a9089856634fe8ef92ac9
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-04-20 13:46:40 +0200

    switch to tab navigation

--- src/containers/Pages/Products.tsx
+++ src/containers/Pages/Products.tsx
@@ -2,39 +2,64 @@ import {Box, Stack, Typography, useTheme} from '@mui/material';
 import {getProducts} from '../../api/urbaninfusion/public/products';
 import {ProductsList} from '../../components/Pages/Products/ProductsList';
 import {useParams} from 'react-router-dom';
-import SideNavigation from '../../components/SideNavigation';
 import Page from '../../components/Wrappers/Page';
 import {useQuery} from 'react-query';
 import {getCategories} from '../../api/urbaninfusion/public/categories';
-
+import {Category} from "../../api/urbaninfusion/dto/categories-dto";
+import {ReactElement, useState} from "react";
+import TabNavigation, {TabProps} from "../../components/TabNavigation";
+import FreeBreakfastOutlinedIcon from '@mui/icons-material/FreeBreakfastOutlined';
+import CategoryOutlinedIcon from '@mui/icons-material/CategoryOutlined';
 
 export default function Products() {
-    const {id} = useParams();
-    const theme = useTheme();
-
     const {isLoading, data: products} = useQuery(
         'products',
         () => getProducts()
     );
 
-    const {isLoading: isLoadingCategories, data: categories} = useQuery(
+    const {isLoading: isLoadingCategories, data: categories}:
+        { isLoading: boolean, data?: TabProps[] } = useQuery(
         'categories',
-        () => getCategories()
+        async () => {
+            const categories = await getCategories()
+            return categories.reduce(
+                (acc: TabProps[], curr: Category) => {
+                    console.log(curr as Category === Category.TEA);
+                    acc.push({
+                        name: curr,
+                        icon: getIconForCategory(curr as Category)
+                    })
+                    return acc;
+                }, [])
+        }
     );
+
+    const [tab, setTab] = useState<number>(0);
+
+    const {id} = useParams();
+    const theme = useTheme();
+
+    const getIconForCategory = (category: Category): ReactElement => {
+        switch (category) {
+            case Category.TEA:
+                return <FreeBreakfastOutlinedIcon/>
+            case Category.ACCESSORIES:
+                return <CategoryOutlinedIcon/>
+            default:
+                return <></>
+        }
+    }
 
     return (
         <>
             <Page isLoading={isLoading || isLoadingCategories}>
-                <Box sx={{
-                    display: 'flex',
-                    width: '100%'
-                }}>
+                <Box>
                     {
                         categories && (
-                            <SideNavigation
-                                items={categories}
-                                header={'Products'}
-                                path={'products'}
+                            <TabNavigation
+                                tabs={categories}
+                                currentTab={tab}
+                                onChange={(newValue) => setTab(newValue)}
                             />
                         )
                     }
