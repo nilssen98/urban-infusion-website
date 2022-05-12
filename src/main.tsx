2c9aa3852718a9618b28ae2fd1f68c11529caf67
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-05-12 21:38:15 +0200

    remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,8 @@ import {Provider} from 'react-redux';
 import {store} from './state/store';
 
 ReactDOM.render(
-    <React.StrictMode>
         <Provider store={store}>
             <App/>
-        </Provider>
-    </React.StrictMode>,
+        </Provider>,
     document.getElementById('root')
 );
