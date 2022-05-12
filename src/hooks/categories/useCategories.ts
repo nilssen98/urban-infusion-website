5a42e21f52be22e5d17432ab30e34b8be4d04f48
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-05-12 13:46:07 +0200

    add categories react query hook

---
+++ src/hooks/categories/useCategories.ts
@@ -0,0 +1,10 @@
+import {useQuery, UseQueryResult} from 'react-query';
+import {CategoriesDto} from '../../api/urbaninfusion/dto/categories-dto';
+import {getCategories} from '../../api/urbaninfusion/public/categories';
+
+export default function useCategories(): UseQueryResult<CategoriesDto> {
+    return useQuery(
+        'categories',
+        () => getCategories()
+    );
+}
