b336ac62f8186a2dd624511411c8cbef4a8665be
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-05-15 22:10:23 +0200

    finish login logic

--- src/containers/Pages/Login.tsx
+++ src/containers/Pages/Login.tsx
@@ -1,14 +1,64 @@
-import {Button, InputAdornment, Paper, Stack, TextField, Typography, useTheme} from '@mui/material';
+import {Alert, Button, InputAdornment, Paper, Snackbar, Stack, TextField, Typography, useTheme} from '@mui/material';
 import Page from '../../components/Wrappers/Page';
-import {NavLink} from 'react-router-dom';
+import {NavLink, useNavigate} from 'react-router-dom';
 import Background from '../../assets/images/teashop-background.jpg';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import PasswordOutlinedIcon from '@mui/icons-material/PasswordOutlined';
+import {login} from '../../api/urbaninfusion/public/login';
+import {useEffect, useState} from 'react';
+import {RootState} from '../../state/store';
+import {connect} from 'react-redux';
+import {userSlice} from '../../state/slices/user';
 
-export default function Login() {
+const mapStateToProps = (state: RootState) => {
+    return {
+        isAuthenticated: state.user.jwt !== undefined,
+    };
+};
+
+const mapDispatchToProps = {
+    setJwtToken: userSlice.actions.setJwtToken,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
+
+type Props = ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps;
+
+function Login(props: Props) {
+    const [error, setError] = useState<boolean>(false);
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
     const theme = useTheme();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (props.isAuthenticated) {
+            navigate('/');
+        }
+    }, [props.isAuthenticated]);
+
+    const handleLogin = async () => {
+        await login({
+            username: username,
+            password: password,
+        })
+            .then(e => {
+                props.setJwtToken(e);
+            })
+            .catch(_ => setError(true));
+    };
+
     return (
         <>
+            <Snackbar
+                open={error}
+                autoHideDuration={6000}
+                onClose={() => setError(false)}
+                anchorOrigin={{vertical: 'top', horizontal: 'center'}}
+            >
+                <Alert severity={'error'}>Invalid username or password!</Alert>
+            </Snackbar>
             <Page>
                 <Stack
                     sx={{
@@ -26,6 +76,8 @@ export default function Login() {
                             <Stack width={'100%'} spacing={4}>
                                 <TextField
                                     required
+                                    value={username}
+                                    onChange={(event) => setUsername(event.target.value)}
                                     label={'Username'}
                                     InputProps={{
                                         endAdornment: (
@@ -37,6 +89,8 @@ export default function Login() {
                                 />
                                 <TextField
                                     required
+                                    value={password}
+                                    onChange={(event) => setPassword(event.target.value)}
                                     label={'Password'}
                                     type={'password'}
                                     InputProps={{
@@ -49,6 +103,7 @@ export default function Login() {
                                 />
                             </Stack>
                             <Button
+                                onClick={() => handleLogin()}
                                 variant={'contained'}
                                 sx={{width: '100%'}}
                             >
