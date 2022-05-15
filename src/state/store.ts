30828c025651861b389c8c3a0abc70a1efcd3265
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-05-15 19:21:56 +0200

    add redux devtool

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,18 +1,18 @@
 import {combineReducers, createStore, EnhancedStore} from '@reduxjs/toolkit';
 import {persistReducer, persistStore} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import {UserPreferences, userPreferencesSlice} from './slices/userPreferences';
+import {User, userSlice} from './slices/user';
 import {Cart, cartSlice} from './slices/cart';
 
 // Root state that combines all the state types from the slices
 export interface RootState {
-    userPreferences: UserPreferences;
+    user: User;
     cart: Cart;
 }
 
 // Root reducers that combines all the available reducers
 const rootReducer = combineReducers({
-    userPreferences: userPreferencesSlice.reducer,
+    user: userSlice.reducer,
     cart: cartSlice.reducer
 });
 
@@ -25,6 +25,9 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // Create the store instance in which we specify our reducers
 // In this case, its the combination of all our reducers
-export const store: EnhancedStore<RootState> = createStore(persistedReducer);
+export const store: EnhancedStore<RootState> = createStore(
+    persistedReducer,
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export const persistor = persistStore(store);
