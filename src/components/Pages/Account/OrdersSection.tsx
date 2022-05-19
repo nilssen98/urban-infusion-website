b2bf3cc6547c98d6e0c484bfd43969678945b8b4
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-05-19 03:16:12 +0200

    improve order styles

--- src/components/Pages/Account/OrdersSection.tsx
+++ src/components/Pages/Account/OrdersSection.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import {OrderDto} from '../../../api/urbaninfusion/dto/order-dto';
+import {OrderDto, OrderStatus} from '../../../api/urbaninfusion/dto/order-dto';
 import {Divider, Paper, Stack, Typography} from '@mui/material';
+import HourglassBottomOutlinedIcon from '@mui/icons-material/HourglassBottomOutlined';
+import SendAndArchiveOutlinedIcon from '@mui/icons-material/SendAndArchiveOutlined';
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+import DoneOutlineOutlinedIcon from '@mui/icons-material/DoneOutlineOutlined';
 
 interface Props {
     orders: OrderDto[];
@@ -8,6 +12,27 @@ interface Props {
 
 export default function OrdersSection(props: Props) {
     const {orders} = props;
+
+    const getStatusIcon = (status: OrderStatus) => {
+        switch (status) {
+            case OrderStatus.IDLE:
+                return (
+                    <HourglassBottomOutlinedIcon color={'disabled'}/>
+                );
+            case OrderStatus.PROCESSING:
+                return (
+                    <SendAndArchiveOutlinedIcon color={'info'}/>
+                );
+            case OrderStatus.SENT:
+                return (
+                    <SendOutlinedIcon color={'warning'}/>
+                );
+            case OrderStatus.DELIVERED:
+                return (
+                    <DoneOutlineOutlinedIcon color={'success'}/>
+                );
+        }
+    };
 
     return (
         <>
@@ -19,22 +44,31 @@ export default function OrdersSection(props: Props) {
                                 <Stack p={4} direction={'row'}>
                                     <Typography flex={1} fontWeight={600}>{order.date}</Typography>
                                     <Typography flex={1} textAlign={'right'} fontWeight={600}>Products</Typography>
-                                    <Typography flex={1} textAlign={'right'} fontWeight={600}>Total ${order.totalPrice}</Typography>
+                                    <Typography flex={1} textAlign={'right'} fontWeight={600}>Total
+                                        ${order.totalPrice}
+                                    </Typography>
                                 </Stack>
                                 <Divider/>
                                 <Stack p={4} direction={'row'}>
-                                    <Typography flex={1}>{order.status}</Typography>
+                                    <Stack alignItems={'center'} direction={'row'} spacing={2}>
+                                        {getStatusIcon(order.status)}
+                                        <Typography textTransform={'capitalize'} flex={1}>{order.status.toLowerCase()}</Typography>
+                                    </Stack>
                                     <Stack flex={1} spacing={2}>
                                         {
                                             order.products.map(({product, quantity}) => (
-                                                <Typography textAlign={'right'} key={product.id}>{product.title} x{quantity}</Typography>
+                                                <Typography textAlign={'right'} key={product.id}>
+                                                    {product.title} x{quantity}
+                                                </Typography>
                                             ))
                                         }
                                     </Stack>
                                     <Stack flex={1} spacing={2}>
                                         {
                                             order.products.map(({product, quantity}) => (
-                                                <Typography textAlign={'right'} key={product.id}>${product.price * quantity}</Typography>
+                                                <Typography textAlign={'right'} key={product.id}>
+                                                    ${product.price * quantity}
+                                                </Typography>
                                             ))
                                         }
                                     </Stack>
