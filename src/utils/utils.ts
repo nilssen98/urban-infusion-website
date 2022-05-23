0ac04b7d4df0600020c4d28e56b5ab49e2eb3be4
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-05-24 01:16:13 +0200

    add enum utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,3 +6,21 @@ export function hexToRgb(hex: string): number[] {
         .slice(1, 4)
         .map(s => parseInt(s, 16));
 }
+
+function isStringEnum<T extends Record<string, string | number>>(t: T): boolean {
+    return typeof Object.values(t).pop() === 'string';
+}
+
+export function enumKeys<T extends Record<string, string | number>>(t: T): (keyof T)[] {
+    return isStringEnum(t) ? Object.keys(t) : Object.keys(t).slice(0, Object.keys(t).length / 2);
+}
+
+export function enumValues<T extends Record<string, string>>(t: T): string[];
+export function enumValues<T extends Record<string, string | number>>(t: T): number[];
+export function enumValues<T extends Record<string, string | number>>(t: T): string[] | number[] {
+    if (isStringEnum(t)) {
+        return Object.values(t) as string[];
+    } else {
+        return Object.values(t).slice(Object.values(t).length / 2) as number[];
+    }
+}
