1d1ae0c4032bade5625dae40b3ecc7440339edde
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-05-26 01:47:06 +0200

    make images lazy load

--- src/components/Cards/ProductCard.tsx
+++ src/components/Cards/ProductCard.tsx
@@ -43,7 +43,7 @@ export default function ProductCard(props: Props) {
                                         justifyContent={'center'}
                                         sx={{cursor: 'pointer'}}
                                     >
-                                        <img src={props.img} style={{width: '100%', height: 175}} alt={''}/>
+                                        <img loading={'lazy'} src={props.img} style={{width: '100%', height: 175}} alt={''}/>
                                     </Stack>
                                 </UnstyledLink>
                                 <Divider flexItem/>
