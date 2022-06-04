6bd39c23982e10e772592a8cff8f095477720ffa
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-06-04 22:51:35 +0200

    redirect to login when not authenticated

--- src/containers/Pages/Checkout.tsx
+++ src/containers/Pages/Checkout.tsx
@@ -1,7 +1,36 @@
 import Page from '../../components/Wrappers/Page';
 import Section from '../../components/Wrappers/Section';
+import {userSlice} from '../../state/slices/user';
+import {connect} from 'react-redux';
+import {ReactElement, useEffect} from 'react';
+import {RootState} from '../../state/store';
+import {useNavigate} from 'react-router-dom';
 
-export default function Checkout() {
+const mapStateToProps = (state: RootState) => {
+    return {
+        isAuthenticated: state.user.jwt !== undefined,
+    };
+};
+
+const mapDispatchToProps = {
+    toggleTheme: userSlice.actions.toggleTheme,
+    setJwtToken: userSlice.actions.setJwtToken,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
+
+type Props = ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps & {
+    children?: ReactElement;
+};
+
+function Checkout(props: Props) {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!props.isAuthenticated) {
+            navigate('/login');
+        }
+    }, []);
 
     return (
         <>
