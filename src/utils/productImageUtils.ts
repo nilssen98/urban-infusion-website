9211e3e8b01353ce77976e40f460dd402055ab5c
Marko19907 <marko.stancic23@gmail.com>
2022-06-04 17:05:18 +0200

    Added a fallback product image

--- src/utils/productImageUtils.ts
+++ src/utils/productImageUtils.ts
@@ -1,6 +1,7 @@
 import {baseUrl} from '../api/urbaninfusion/public/public';
+import noImage from '../assets/images/no-image.svg';
 
-export const defaultProductImageURL = 'https://i.imgur.com/ZG4W7Le.jpg';
+export const defaultProductImageURL = noImage;
 
 const productImagesEndpoint = 'product-images';
 
