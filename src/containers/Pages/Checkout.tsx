31633a0e64b0347de6e4fe0b2b00207545ad372c
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-06-04 22:41:26 +0200

    add structure to checkout page

--- src/containers/Pages/Checkout.tsx
+++ src/containers/Pages/Checkout.tsx
@@ -1,6 +1,15 @@
+import Page from '../../components/Wrappers/Page';
+import Section from '../../components/Wrappers/Section';
+
 export default function Checkout() {
+
     return (
         <>
+            <Page>
+                <Section>
+
+                </Section>
+            </Page>
         </>
     );
 }
