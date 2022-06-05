6213532c729f0cc6efddf61d90457ece8a0087ce
Kasper Nilssen <kasper.nilssen1@gmail.com>
2022-06-05 14:51:57 +0200

    start refactoring product page

--- src/containers/Pages/Product.tsx
+++ src/containers/Pages/Product.tsx
@@ -1,6 +1,6 @@
 import {useNavigate, useParams} from 'react-router-dom';
 import Section from '../../components/Wrappers/Section';
-import {Button, Collapse, Stack, Typography, useTheme} from '@mui/material';
+import {Button, Chip, Collapse, Divider, Stack, Typography, useTheme} from '@mui/material';
 import Page from '../../components/Wrappers/Page';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import PictureBox from '../../components/PictureBox';
@@ -10,10 +10,104 @@ import {useEffect, useState} from 'react';
 import CommentForm from '../../components/Pages/Product/CommentForm';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import useProduct from '../../hooks/products/useProduct';
-import {round} from 'lodash-es';
+import {capitalize, round} from 'lodash-es';
 import {getProductImageURL} from '../../api/urbaninfusion/public/products';
+import Counter from '../../components/Counter';
+import {RootState} from '../../state/store';
+import {cartSlice} from '../../state/slices/cart';
+import {connect} from 'react-redux';
 
-export default function Product() {
+const mapStateToProps = (state: RootState) => {
+    return {
+        cart: state.cart.items,
+    };
+};
+
+const mapDispatchToProps = {
+};
+
+type Props = ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps & {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Product);
+
+function Product() {
+    const {id} = useParams();
+    const navigate = useNavigate();
+    const theme = useTheme();
+
+    const {isLoading, isError, data: product} = useProduct(id);
+
+    useEffect(() => {
+        if (isError) {
+            navigate('/products');
+        }
+    }, [isError]);
+
+    const handleIncrement = () => {
+    };
+    const handleDecrement = () => {
+    };
+
+    return (
+        <>
+            <Page isLoading={isLoading}>
+                <Section>
+                    {
+                        product && (
+                            <Stack width={'100%'} spacing={8}>
+                                <Stack direction={{md: 'row', xs: 'column'}} spacing={8}>
+                                    <Stack flex={1} alignItems={'center'} justifyContent={'center'}>
+                                        <img
+                                            style={{height: 320, width: 320}}
+                                            src={getProductImageURL(product.id)}
+                                            alt={''}
+                                        />
+                                    </Stack>
+                                    <Stack spacing={4} flex={1} justifyContent={'space-between'}>
+                                        <Chip
+                                            label={capitalize(product.category)}
+                                            sx={{alignSelf: 'start'}}
+                                            variant={'outlined'}
+                                        />
+                                        <Typography variant={'h3'}>{product.title}</Typography>
+                                        <Stack direction={'row'} alignItems={'center'} spacing={1}>
+                                            <Typography variant={'h5'}>${product.price}</Typography>
+                                            <Typography variant={'body2'} color={theme.palette.text.secondary}>
+                                                {product.weight ? `/ ${product.weight}` : null}
+                                            </Typography>
+                                        </Stack>
+                                        <Typography>{product.description}</Typography>
+                                        <Stack direction={'row'} alignItems={'center'} spacing={4}>
+                                            <Counter
+                                                sx={{alignSelf: 'end'}}
+                                                count={0}
+                                                onIncrement={handleIncrement}
+                                                onDecrement={handleDecrement}
+                                            />
+                                            <Button
+                                                startIcon={<AddShoppingCartIcon/>}
+                                                variant={'contained'}
+                                                size={'large'}
+                                            >
+                                                Add to cart
+                                            </Button>
+                                        </Stack>
+                                    </Stack>
+                                </Stack>
+                                <Divider/>
+                                <Stack>
+                                    yo
+                                </Stack>
+                            </Stack>
+                        )
+                    }
+                </Section>
+            </Page>
+        </>
+    );
+}
+
+function Product2() {
     const {id} = useParams();
     const theme = useTheme();
     const navigate = useNavigate();
@@ -50,7 +144,7 @@ export default function Product() {
                                         alignItems={'center'}
                                         sx={{
                                             paddingLeft: {xs: 0, md: 6, lg: 28},
-                                    }}
+                                        }}
                                     >
                                         {
                                             data.discount !== 0 && (
